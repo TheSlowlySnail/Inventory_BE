c9b03f4a44fc19682a83b60f4dc071ef1bfee399
Helge Meiering <helge.meiering@gmail.com>
2018-04-20 13:10:39 +0200

    add migrations

---
+++ database/migrations/2018_04_20_110030_create_item_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->string('barcode');
+            $table->primary('barcode');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('type')->nullable();
+            $table->string('room')->nullable();
+            $table->string('status')->nullable();
+            $table->string('annotation')->nullable();
+            $table->string('image')->nullable();
+            $table->integer('lend')->unsigned()->nullable();
+            $table->string('manufactor')->nullable();
+            $table->timestamps();
+        });
+//        Schema::table('items',function ($table){
+//            $table->foreign('lend')->references('id')->on('lending');
+//
+//        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
---
+++ database/migrations/2018_04_20_110040_create_lending_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLendingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lending', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->integer('personid')->unsigned();
+            $table->string('barcode');
+            $table->dateTime('startdate');
+            $table->dateTime('enddate');
+            $table->timestamps();
+        });
+//        Schema::table('lending',function ($table){
+//            $table->foreign('barcode')->references('barcode')->on('items');
+//            $table->foreign('personid')->references('personid')->on('persons');
+//
+//        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lending');
+    }
+}
